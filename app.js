085d89d9a4a01ad8c751dca64854afd1e9b92553
Brandon Leichty <brandonl@me.com>
2018-06-05 10:54:23 -0500

    change 'res.render' to 'res.json' for Postman use

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ error: err.message || 'Something went wrong' });
 });
 
 module.exports = app;
