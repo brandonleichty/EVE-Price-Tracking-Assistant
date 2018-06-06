60b71138a8f948ff4f5f3ffa5e9b2f27ff7409f7
Brandon Leichty <brandonl@me.com>
2018-06-05 23:05:05 -0500

    add Nodemailer to send test emails

--- controllers/messagingController.js
+++ controllers/messagingController.js
@@ -1,3 +1,6 @@
+'use strict';
+const nodemailer = require('nodemailer');
+
 // Twilio Credentials
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
@@ -8,15 +11,61 @@ const client = require('twilio')(accountSid, authToken);
 	This middleware will use SendGrid to keep the user up-to-date via email.
 */
 exports.email = (req, res, next) => {
-	// If the 'Update via email" checkbox wasn't checked, move to the next middleware.
+	// If the 'Update via email" checkbox wasn't been checked, move to the next middleware.
 	if (req.body.email !== 'on') {
 		return next();
-	}
 
-	// TODO: Insert code to send email to user here.
+		// Else, if the email checkbox HAS been checked, send an email.
+	} else {
+
+
+		/*
+			TODO: Insert code to send email to user here.
+		*/
+
+		// Create reusable transporter object using the default SMTP transport
+		const transporter = nodemailer.createTransport({
+			host: 'smtp.ethereal.email',
+			port: 587,
+			auth: {
+				user: 'ky4u75gya7x3tvv6@ethereal.email',
+				pass: 'vScTWHRm6ZfczyXZW3'
+			}
+		});
+
+		// Setup email data with unicode symbols
+		const mailOptions = {
+			from: '"EVE 🤖" <eve@evetracking.com>', // sender address
+			to: `${req.body.address}`,
+			subject: `EVE is now watching ${req.body.title} from ${req.body.company}`,
+			text: `I've started watching the ${req.body.title} from ${req.body.company}. The price is currently ${req.body.price}.
+
+			I'll update you if there are any changes.
+			-EVE`,
+			html:
+`<p>I've started watching the ${req.body.title} from ${req.body.company}. The price is currently ${req.body.price}.<br><br>
+I'll update you if there are any changes.<br>
+<b>-EVE</b></p>`
+		};
+
+		// send mail with defined transport object
+		transporter.sendMail(mailOptions, (error, info) => {
+			if (error) {
+				return console.log(error);
+			}
+			console.log('Message sent: %s', info.messageId);
+			// Preview only available when sending through an Ethereal account
+			console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+			// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+			// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+		});
+	}
 
 	return next();
 };
+
+
 
 /*
 	This middleware uses Twilio to send sms updates to the user.
@@ -30,7 +79,9 @@ exports.sms = (req, res, next) => {
 			to: phone,
 			from: process.env.TWILIO_PHONE_NUMBER,
 			mediaUrl: `${req.body.image}`,
-			body: `I've started watching the ${req.body.title} from ${req.body.company}. The price is currently ${req.body.price}.
+			body: `I've started watching the ${req.body.title} from ${
+				req.body.company
+			}. The price is currently ${req.body.price}.
 
 I'll update you if there are any changes.
 -EVE`
@@ -38,6 +89,5 @@ I'll update you if there are any changes.
 		.then(message => console.log(message.sid))
 		.done();
 
-	console.log(`An email was sent to ${req.body.address}, and a text to ${req.body.tel}.`)
-	res.send(`An email was sent to ${req.body.address}, and a text to ${req.body.tel}.`);
+		return next();
 };
